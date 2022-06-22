d02cf99d133dbb63c2ad9954637b35f1c543527e
Mason Keeney <mason.keeney68@gmail.com>
2022-06-22 10:03:19 -0600

    Created Boot REST project, set JPA Dependency

---
+++ ToolDragon/src/main/java/com/skilldistillery/tooldragon/ToolDragonApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.tooldragon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolDragonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolDragonApplication.class, args);
+	}
+
+}
