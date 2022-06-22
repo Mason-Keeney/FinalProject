3eba233e2beb637efa57f74dc570c742253636b7
Devan Mapp <Devan.R.Mapp@gmail.com>
2022-06-22 13:41:36 -0600

    angular added, auth and user.ts

---
+++ ngToolDragon/src/app/models/user.ts
@@ -0,0 +1,21 @@
+export class User {
+
+  id: number | null;
+  username: string|null;
+  password: string|null;
+  role: string | null;
+
+
+  constructor(
+    id: number | null = 0,
+    username: string|null = '',
+    password: string|null = '',
+    role: string | null = ''
+
+  ){
+    this.id = id;
+    this.username = username;
+    this.password = password;
+    this.role = role;
+  }
+}
