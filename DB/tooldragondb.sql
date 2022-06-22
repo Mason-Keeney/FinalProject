c5f8b7f1548364c543e470d5d11d609f1105e077
Angel <angel1179008@gmail.com>
2022-06-22 14:50:32 -0600

    Fixed postman authenticate

--- DB/tooldragondb.sql
+++ DB/tooldragondb.sql
@@ -43,7 +43,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `last_name` VARCHAR(45) NULL,
   `username` VARCHAR(45) NOT NULL,
   `password` VARCHAR(200) NOT NULL,
-  `active` TINYINT NOT NULL,
+  `active` TINYINT NULL,
   `image_url` VARCHAR(2000) NULL,
   `description` TEXT NULL,
   `address_id` INT NULL,
@@ -51,6 +51,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `updated_at` DATETIME NULL,
   `last_login` DATETIME NULL,
   `background_image_url` VARCHAR(2000) NULL,
+  `enabled` TINYINT NOT NULL,
   `role` VARCHAR(45) NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `username_UNIQUE` (`username` ASC),
@@ -546,7 +547,7 @@ SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `tooldragondb`;
-INSERT INTO `user` (`id`, `first_name`, `last_name`, `username`, `password`, `active`, `image_url`, `description`, `address_id`, `created_at`, `updated_at`, `last_login`, `background_image_url`, `role`) VALUES (1, 'Angel', 'Casillas', 'acadmin', '$2a$10$kR5d.m9Wj2X/xGqNp4o.8u.fXYMBKHlxzOZH3tAkTApbnZIQE88lO', 1, NULL, 'desc', NULL, NULL, NULL, NULL, NULL, 'role_admin');
+INSERT INTO `user` (`id`, `first_name`, `last_name`, `username`, `password`, `active`, `image_url`, `description`, `address_id`, `created_at`, `updated_at`, `last_login`, `background_image_url`, `enabled`, `role`) VALUES (1, 'Angel', 'Casillas', 'acadmin', '$2a$10$iLZuVQKCjz0QH0xGZFuUeeXuZm7orxQzNlMCKINdR/DvvJIvwX0he', 1, NULL, 'desc', NULL, NULL, NULL, NULL, NULL, 1, 'role_admin');
 
 COMMIT;
 
