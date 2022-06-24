ed137f5c46bac60a6f3fe59e4318b7212b5bb116
Devan Mapp <Devan.R.Mapp@gmail.com>
2022-06-24 09:25:49 -0600

    added more models and finished adding properties to user

--- ngToolDragon/src/app/models/user.ts
+++ ngToolDragon/src/app/models/user.ts
@@ -3,19 +3,47 @@ export class User {
   id: number | null;
   username: string|null;
   password: string|null;
+  enabled: boolean;
   role: string | null;
+  firstName: string | null;
+  lastName: string | null;
+  imageUrl: string | null;
+  description: string | null;
+  createdAt: Date | null;
+  updatedAt: Date | null;
+  lastLogin: Date | null;
+  backgroundImageUrl: string | null;
+
 
 
   constructor(
     id: number | null = 0,
-    username: string|null = '',
-    password: string|null = '',
-    role: string | null = ''
+    username: string = '',
+    password: string = '',
+    enabled: boolean = true,
+    role: string | null = '',
+    firstName: string | null = '',
+    lastName: string | null = '',
+    imageUrl: string | null = '',
+    description: string | null = '',
+    createdAt: Date | null = null,
+    updatedAt: Date | null = null,
+    lastLogin: Date | null = null,
+    backgroundImageUrl: string | null = ''
 
   ){
     this.id = id;
     this.username = username;
     this.password = password;
+    this.enabled = enabled;
     this.role = role;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+    this.lastLogin = lastLogin;
+    this.backgroundImageUrl = backgroundImageUrl;
   }
 }
