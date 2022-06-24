24148cede95fdd5536380fe656117feebe19f564
Devan Mapp <Devan.R.Mapp@gmail.com>
2022-06-24 11:50:46 -0600

    finished out address through project models

--- ngToolDragon/src/app/models/address.ts
+++ ngToolDragon/src/app/models/address.ts
@@ -1,2 +1,27 @@
 export class Address {
+  id: number | null;
+  street1: string | null;
+  street2: string | null;
+  city: string | null;
+  state: string | null;
+  postalCode: string | null;
+  active: boolean;
+
+  constructor(
+    id: number | null = 0,
+    street1: string | null = '',
+    street2: string | null = '',
+    city: string | null = '',
+    state: string | null = '',
+    postalCode: string | null = '',
+    active: boolean = true
+  ) {
+    this.id = id;
+    this.street1 = street1;
+    this.street2 = street2;
+    this.city = city;
+    this.state = state;
+    this.postalCode = postalCode;
+    this.active = active;
+  }
 }
---
+++ ngToolDragon/src/app/models/project-tool.spec.ts
@@ -0,0 +1,7 @@
+import { ProjectTool } from './project-tool';
+
+describe('ProjectTool', () => {
+  it('should create an instance', () => {
+    expect(new ProjectTool()).toBeTruthy();
+  });
+});
