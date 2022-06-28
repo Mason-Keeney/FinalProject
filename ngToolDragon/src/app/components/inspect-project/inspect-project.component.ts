9eb7d51820dc59d56822adef8d40b25988722588
Angel <angel1179008@gmail.com>
2022-06-28 15:46:00 -0600

    pushed project tool list

--- ngToolDragon/src/app/components/inspect-project/inspect-project.component.ts
+++ ngToolDragon/src/app/components/inspect-project/inspect-project.component.ts
@@ -1,4 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Tool } from 'src/app/models/tool';
+import { User } from 'src/app/models/user';
+import { AuthService } from 'src/app/services/auth.service';
+import { ProjectService } from 'src/app/services/project.service';
+import { ToolService } from 'src/app/services/tool.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-inspect-project',
@@ -6,12 +12,46 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./inspect-project.component.css']
 })
 export class InspectProjectComponent implements OnInit {
+  user: User = new User;
+  toolListFull: Tool[] = [];
+  toolList: Tool[] = [];
 
   @Input() project: any;
 
-  constructor() { }
+  constructor(
+    private projectServ: ProjectService,
+    private userServ: UserService,
+    private authService: AuthService,
+    private toolService: ToolService,
+  ) { }
 
   ngOnInit(): void {
+    this.toolIndex();
+    this.authenticateUser();
   }
+
+  authenticateUser(){
+    this.authService.authenticateUser().subscribe({
+      next: (result) =>{
+        this.user = result;
+      },
+      error: (problem) => {
+        console.log(problem);
+      }
+    })
+  }
+
+  toolIndex() {
+    this.toolService.indexAll().subscribe({
+      next: (result) => {
+        this.toolListFull = result;
+      },
+      error: (nojoy) => {
+       console.log('Tool.index(): error retrieving Tools:');
+       console.log(nojoy);
+      },
+    });
+  }
+
 
 }
